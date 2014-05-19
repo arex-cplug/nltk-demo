db3ff18b3264d49823b5d599cc26f03d67b100d4
Alexandre Macedo <alexandre.macedo@kekanto.com>
2014-05-19 19:45:20 -0300

    ...

---
+++ classifier.py
@@ -0,0 +1,14 @@
+#/usr/bin/env python
+
+import sys
+
+import nltk
+
+
+def main():
+    train = 
+    classifier = nltk.classify.NaiveBayesClassifier.train(train)
+
+
+if __name__ == '__main__':
+    main()
